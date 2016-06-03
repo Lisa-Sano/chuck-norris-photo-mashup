e00c5bbef873e4bc3c4473e23f1e5ec014366701
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-02 23:46:44 -0700

    added a route/controller method/view for index to get a random joke and show to get a specific joke. index view just prints out the joke and the joke id number for now.

--- app/controllers/mashups_controller.rb
+++ app/controllers/mashups_controller.rb
@@ -1,2 +1,14 @@
+require 'chuck'
+
 class MashupsController < ApplicationController
+
+  def index
+    @joke = ChuckJokeWrapper.random
+  end
+
+  # make a show page for specific jokes (debugging purposes)
+  def show
+    @joke = ChuckJokeWrapper.find_joke(params[:id])
+    render :index
+  end
 end
---
+++ lib/chuck.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+module ChuckJokeWrapper
+  BASE_URL = 'https://api.icndb.com/'
+
+  # gets a random joke from the API
+  # http://api.icndb.com/jokes/random
+  def self.random
+    HTTParty.get(BASE_URL + 'jokes/random?exclude=[explicit]').parsed_response
+  end
+
+  def self.find_joke(id)
+    HTTParty.get(BASE_URL + "jokes/#{id}").parsed_response
+  end
+end
