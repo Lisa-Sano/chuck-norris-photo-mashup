d5a1cbab26c8b81a036afd42d188635d1fd1f2e1
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-03 02:06:19 -0700

    index page shows the joke text and a flickr photo

--- app/controllers/mashups_controller.rb
+++ app/controllers/mashups_controller.rb
@@ -1,10 +1,12 @@
 require 'chuck'
+require 'tagger'
 
 class MashupsController < ApplicationController
 
   def index
-    @joke = ChuckJokeWrapper.random
-    @photo = Photo.find_photo("sky")
+    @joke = ChuckJokeWrapper.random["value"]["joke"]
+    @search_term = Tagger.get_keywords(@joke)
+    @photo = Photo.find_photo(@search_term)
   end
 
   # make a show page for specific jokes (debugging purposes)
