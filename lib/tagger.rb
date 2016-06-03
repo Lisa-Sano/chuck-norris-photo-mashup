2c4bffecddff77d106b5c2a324668fcf89777561
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-03 12:39:51 -0700

    searches based on noun phrases instead of just nouns. if no noun phrases are present, returns Chuck Norris as the search term

--- lib/tagger.rb
+++ lib/tagger.rb
@@ -10,7 +10,11 @@ module Tagger
 
     tgr = EngTagger.new
     tagged_joke = tgr.add_tags(search_text)
-    nouns = tgr.get_nouns(tagged_joke).keys
-    nouns.max_by { |noun| noun.length}
+    nouns = tgr.get_noun_phrases(tagged_joke).keys
+    # nouns.max_by { |noun| noun.length}
+    return nouns.sample unless nouns.empty?
+
+    # if there are no nouns pulled out from the joke, return Chuck Norris as photo search term
+    "Chuck Norris"
   end
 end
