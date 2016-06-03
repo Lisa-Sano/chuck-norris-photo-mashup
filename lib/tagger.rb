a5ad9fba235c9d9f7ffe035a4f83010f42d0b7f9
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-03 02:06:03 -0700

    removes punctuation and the name Chuck Norris from the joke, then pulls out the nouns and returns the longest one

---
+++ lib/tagger.rb
@@ -0,0 +1,16 @@
+require 'engtagger'
+
+module Tagger
+  def self.get_keywords(joke)
+    # remove non-word characters from joke
+    search_text = joke.gsub(/\.\,\?!/, '')
+
+    # remove Chuck Norris from the potential search terms
+    search_text.gsub!("Chuck Norris", '')
+
+    tgr = EngTagger.new
+    tagged_joke = tgr.add_tags(search_text)
+    nouns = tgr.get_nouns(tagged_joke).keys
+    nouns.max_by { |noun| noun.length}
+  end
+end
