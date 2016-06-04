c33fbacff65fa2a508b50b6dbd4d8a953998f5a7
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-04 13:07:12 -0700

    updates the tagger method to break up substitution into a different method. fixed a bug that was checking in the wrote place for a & in the chosen noun phrase to decide whether to return default value or not

--- lib/tagger.rb
+++ lib/tagger.rb
@@ -2,19 +2,25 @@ require 'engtagger'
 
 module Tagger
   def self.get_keywords(joke)
-    # remove non-word characters from joke
-    search_text = joke.gsub(/[\.\,\?!"']/, '')
-
-    # remove Chuck Norris from the potential search terms
-    search_text.gsub!("Chuck Norris", '')
+    # remove non-word characters & 'Chuck Norris' from joke
+    search_text = Tagger.substitute(joke)
 
     tgr = EngTagger.new
     tagged_joke = tgr.add_tags(search_text)
-    nouns = tgr.get_noun_phrases(tagged_joke).keys
-    # nouns.max_by { |noun| noun.length}
-    return nouns.sample unless (nouns.empty? || nouns.include? "&")
+    noun_phrases = tgr.get_noun_phrases(tagged_joke).keys
+    # noun_phrases.max_by { |noun| noun.length}
+    a_phrase = noun_phrases.sample
+    return a_phrase unless (a_phrase.nil? || a_phrase.include?("&"))
 
-    # if there are no nouns pulled out from the joke, return Chuck Norris as photo search term
+    # if there are no noun_phrases pulled out from the joke, return Chuck Norris as photo search term
     "Chuck Norris"
+  end
+
+  private
+
+  def self.substitute(joke)
+    alt_text = joke.gsub(/[\.\,\?!"']/, '')
+    alt_text.gsub!("Chuck Norris", '')
+    alt_text
   end
 end
