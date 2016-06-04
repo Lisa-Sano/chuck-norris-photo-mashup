fc83d7a0bbd08173b0ee1c7e7f13406701212f5b
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-04 14:00:53 -0700

    favorites page shows 10 per page

--- app/controllers/mashups_controller.rb
+++ app/controllers/mashups_controller.rb
@@ -23,6 +23,6 @@ class MashupsController < ApplicationController
   end
 
   def favorites
-    @mashups = Mashup.order(favorites: :desc)
+    @mashups = Mashup.order(favorites: :desc).paginate(:page => params[:page], per_page: 10)
   end
 end
