b0ede6391bdb8fa3d6e464d3eb7bf40c60487361
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-04 11:44:26 -0700

    wrote a test for the add_favorite method in the mashup model

--- test/models/mashup_test.rb
+++ test/models/mashup_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class MashupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add_favorite will increase the favorites attribute" do
+    mashup = mashups(:mashup_one)
+    assert_equal 2, mashup.favorites
+
+    mashup.add_favorite
+    assert_equal 3, mashup.favorites
+  end
 end
